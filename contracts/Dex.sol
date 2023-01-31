db47f5af37544efdeb5bd005436d191b59ad0214
Zainab Oufqir <zayna.oufkir@gmail.com>
2023-01-31 23:00:38 +0100

    add contract token and dex

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -1,5 +1,55 @@
 //SPDX-Licence-Identifier: MIT
 
 pragma solidity 0.8.17;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Dex {}
+contract Dex {
+    IERC20 public token;
+    uint price;
+    address owner;
+
+    constructor(IERC20 _token, uint _price) {
+        token = _token;
+        price = _price;
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function sell() external onlyOwner {
+        uint allowance = token.allowance(msg.sender, address(this));
+        require(allowance > 0, "no token allowance");
+        bool sent = token.transferFrom(msg.sender, address(this), allowance);
+        require(sent, "failed to send");
+    }
+
+    function withdrawTokens() external onlyOwner {
+        uint balance = getTokenBalance();
+        token.transfer(msg.sender, balance);
+    }
+
+    function withdrawFunds() external onlyOwner {
+        (bool sent, ) = payable(msg.sender).call{value: address(this).balance}(
+            ""
+        );
+        require(sent, "failed to send");
+    }
+
+    function getPrice(uint tokens) public view returns (uint) {
+        return tokens * price;
+    }
+
+    function buy(uint tokens) external payable {
+        require(tokens <= getTokenBalance(), "not enough tokens");
+        uint _price = getPrice(tokens);
+        require(_price == msg.value, "invalid price");
+        token.transfer(msg.sender, tokens);
+    }
+
+    function getTokenBalance() public view returns (uint) {
+        return token.balanceOf(address(this));
+    }
+}
--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,5 +1,10 @@
 //SPDX-Licence-Identifier: MIT
 
 pragma solidity 0.8.17;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Token {}
+contract Token is ERC20 {
+    constructor(uint supply) ERC20("Token", "tkn") {
+        _mint(msg.sender, supply);
+    }
+}
